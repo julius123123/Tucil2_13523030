03cf22979d7f775f916512fe7d943c51057631ec
Julius <juliusarthur2005@gmail.com>
2025-04-08 14:03:31 +0700

    feat: error calc

--- src/QuadTree.cpp
+++ src/QuadTree.cpp
@@ -1,7 +1,7 @@
 #include "QuadTree.hpp"
 
 QuadTree::QuadTree(int row0, int row1, int col0, int col1, uchar* image, int rows, int cols, int min, double error, int e){
-    root = new QuadNode(row0, row1, col0, col1);
+    root = new QuadNode(row0, row1, col0, col1, 0);
     this->image = image;
     this->rows = rows;
     this->cols = cols;
@@ -12,43 +12,46 @@ QuadTree::QuadTree(int row0, int row1, int col0, int col1, uchar* image, int row
 
 QuadTree::~QuadTree(){
     delete root;
-    std::cout<<"delete quadtree"<<endl;
 }
 
 void QuadTree::BuildTree(QuadNode* node){
+    if (node->depth > depth){
+        depth = node->depth;
+    }
     double varB, varG, varR;
     if (error_calc == 1){
-        varB= Error::variance(image, QuadTree::cols, 0, node->row0, node->col0, node->row1, node->col1);
-        varG = Error::variance(image, QuadTree::cols, 1, node->row0, node->col0, node->row1, node->col1);
-        varR = Error::variance(image, QuadTree::cols, 2, node->row0, node->col0, node->row1, node->col1);
+        varB= Error::variance(image, cols, 0, node->row0, node->col0, node->row1, node->col1);
+        varG = Error::variance(image, cols, 1, node->row0, node->col0, node->row1, node->col1);
+        varR = Error::variance(image, cols, 2, node->row0, node->col0, node->row1, node->col1);
     }
     
     else if (error_calc == 2){
-        varB = Error::MAD(image, QuadTree::cols, 0, node->row0, node->col0, node->row1, node->col1);
-        varG = Error::MAD(image, QuadTree::cols, 1, node->row0, node->col0, node->row1, node->col1);
-        varR = Error::MAD(image, QuadTree::cols, 2, node->row0, node->col0, node->row1, node->col1);
+        varB = Error::MAD(image, cols, 0, node->row0, node->col0, node->row1, node->col1);
+        varG = Error::MAD(image, cols, 1, node->row0, node->col0, node->row1, node->col1);
+        varR = Error::MAD(image, cols, 2, node->row0, node->col0, node->row1, node->col1);
     }
     
     else if (error_calc == 3){
-        varB = Error::MPD(image, QuadTree::cols, 0, node->row0, node->col0, node->row1, node->col1);
-        varG = Error::MPD(image, QuadTree::cols, 1, node->row0, node->col0, node->row1, node->col1);
-        varR = Error::MPD(image, QuadTree::cols, 2, node->row0, node->col0, node->row1, node->col1);
+        varB = Error::MPD(image, cols, 0, node->row0, node->col0, node->row1, node->col1);
+        varG = Error::MPD(image, cols, 1, node->row0, node->col0, node->row1, node->col1);
+        varR = Error::MPD(image, cols, 2, node->row0, node->col0, node->row1, node->col1);
     }
     
     else if (error_calc == 4){
-        varB = Error::entropy(image, QuadTree::cols, 0, node->row0, node->col0, node->row1, node->col1);
-        varG = Error::entropy(image, QuadTree::cols, 1, node->row0, node->col0, node->row1, node->col1);
-        varR = Error::entropy(image, QuadTree::cols, 2, node->row0, node->col0, node->row1, node->col1);
+        varB = Error::entropy(image, cols, 0, node->row0, node->col0, node->row1, node->col1);
+        varG = Error::entropy(image, cols, 1, node->row0, node->col0, node->row1, node->col1);
+        varR = Error::entropy(image, cols, 2, node->row0, node->col0, node->row1, node->col1);
     }
     
     double var = (varB + varG + varR)/3;
-    int size = (node->row1 - node->row0) * QuadTree::cols + node->col1 - node->col0;
+    int size = (node->row1 - node->row0) * (node->col1 - node->col0);
     bool valid = false;
 
-    if (var > QuadTree::error_thres){
-        if (size > QuadTree::min_block && size/4 >= QuadTree::min_block){
+    if (var > error_thres){
+        if (size > min_block && size/4 >= min_block){
             valid = true;
             node->split();
+            leaf += 4;
             for (int i = 0; i < 4; i++){
                 BuildTree(node->child[i]);
             }
@@ -56,14 +59,14 @@ void QuadTree::BuildTree(QuadNode* node){
     }
 
     if (!valid){
-        double avgB = Error::average(image, QuadTree::cols, 0, node->row0, node->col0, node->row1, node->col1);
-        double avgG = Error::average(image, QuadTree::cols, 1, node->row0, node->col0, node->row1, node->col1);
-        double avgR = Error::average(image, QuadTree::cols, 2, node->row0, node->col0, node->row1, node->col1);
+        double avgB = Error::average(image, cols, 0, node->row0, node->col0, node->row1, node->col1);
+        double avgG = Error::average(image, cols, 1, node->row0, node->col0, node->row1, node->col1);
+        double avgR = Error::average(image, cols, 2, node->row0, node->col0, node->row1, node->col1);
         int idx;
         
         for (int i = node->row0; i <= node->row1; i++){
             for (int j = node->col0; j <= node->col1; j++){
-                idx = (i * QuadTree::cols + j) * 3;
+                idx = (i * cols + j) * 3;
                     image[idx+0] = avgB;
                     image[idx+1] = avgG;
                     image[idx+2] = avgR;
--- src/QuadTree.hpp
+++ src/QuadTree.hpp
@@ -6,11 +6,15 @@
 class QuadTree{
 public:
     QuadNode* root;
-    static int min_block;
-    static double error_thres;
-    static int rows;
-    static int cols;
-    QuadTree(int row0, int row1, int col0, int col1, uchar* image);
+    int min_block;
+    double error_thres;
+    int rows;
+    int cols;
+    uchar* image;
+    int error_calc;
+    int depth = 0;
+    int leaf = 0;
+    QuadTree(int row0, int row1, int col0, int col1, uchar* image,int rows,int cols, int min_block, double error, int error_calc);
     ~QuadTree();
     void BuildTree(QuadNode* node);
 };
